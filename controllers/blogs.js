a045a61d0ff4964c5df21eeae81bf7534046fed8
Sanket <barcedude@gmail.com>
2021-08-05 19:55:30 -0400

    initial commit

---
+++ controllers/blogs.js
@@ -0,0 +1,25 @@
+const { modelNames } = require('mongoose')
+
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+        })
+})
+
+blogRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+  
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+})
+
+
+module.exports = blogRouter
